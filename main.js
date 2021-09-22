b7d16532673a9a59cac5b8512f00444f688505b4
Ronav Sharma <74993217+MasterProatadvacedlevel12@users.noreply.github.com>
2021-09-21 18:56:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    img(video,0,0,300,300);
 }
